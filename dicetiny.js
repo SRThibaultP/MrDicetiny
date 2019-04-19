0b42bf555bda786eeff5179e8acfc84a655d2165
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-04-19 14:12:54 +0200

    Update code + dependency Fix #2 Fix #4

--- dicetiny.js
+++ dicetiny.js
@@ -6,93 +6,28 @@ var dicetiny = new Discord.Client();
 
 dicetiny.on("message", function(message) {
   var channel_id = message.channel.id;
-  var match_data = message.content.match(/^\/roll ([1-9][0-9]*)d([1-9][0-9]*)s([1-9][0-9]*)/);
+  var match_data = message.content.match(/^\/rolls d([1-9][0-9]*)s([1-9][0-9]*)/);
+  var message_content = "";
+  var user = message.author;
+  var sucessValCri = 5;
+  var sucessValCriSIX = 1;
+  var sucessTXT = "Succès "
+  var sucessGIF = "LIEN DU GIF SUCCES"
+  var sucessGIFCri = "LIEN DU GIF SUCESS CRITIQUE";
+  var echecValCri = 95;
+  var echecValCriSIX = 6;
+  var echecTXT = "Échec "
+  var echecGIF = "LIEN DU GIF ECHEC"
+  var echecGIFCri = "LIEN DU GIF ECHEC CRITIQUE";
+  var critique = "critique "
+  var chanceux = "";
 
   if(match_data) {
-    var n_dice = parseInt(match_data[1], 10);
-    var n_sides = parseInt(match_data[2], 10);
-    var n_stat = parseInt(match_data[3], 10);
+    var n_sides = parseInt(match_data[1], 10);
+    var n_stat = parseInt(match_data[2], 10);
+    var sum = roller.roll(1, n_sides);
 
-    if(n_dice > 9000) {
-      dicetiny.sendMessage(channel_id, "It's over nine thousand");
-      return;
-    }
-
-    console.log("Lancer de " + n_dice + " dé à " + n_sides + " faces avec des stats de " + n_stat);
-    var dice = roller.roll(n_dice, n_sides);
-
-    var message_content = "";
-    var roll_user = message.author;
-    var sum;
-    var sucessValCri = 6;
-    var sucessValCriSIX = 1;
-    var sucessTXT = "Succès "
-    var sucessGIF = "LIEN DU GIF SUCCES"
-    var sucessGIFCri = "LIEN DU GIF SUCESS CRITIQUE";
-    var echecValCri = 94;
-    var echecValCriSIX = 6;
-    var echecTXT = "Échec "
-    var echecGIF = "LIEN DU GIF ECHEC"
-    var echecGIFCri = "LIEN DU GIF ECHEC CRITIQUE";
-    var critique = "critique "
-    var chanceux = "";
-
-    if(n_dice > 1) {
-      sum = dice.reduce(function(prev, curr) {
-        return prev + curr;
-      });
-
-      if (n_sides == 6) {
-        if(sum == sucessValCriSIX) {
-          chanceux = " | " + "**" + sucessTXT + critique + "**" + sucessGIFCri;
-        }
-
-        else if (sum == echecValCriSIX) {
-          chanceux = " | " + "**" + echecTXT + critique + "**" + echecGIFCri;
-        }
-
-        else if (sum <= n_stat) {
-          chanceux = " | " + "**" + sucessTXT + "**" + sucessGIF;
-        }
-
-        else if (sum > n_stat) {
-          chanceux = " | " + "**" + echecTXT + "**" + echecGIF;
-        }
-      }
-
-      else if (n_sides == 100) {
-        if(sum < sucessValCri) {
-          chanceux = " | " + "**" + sucessTXT + critique + "**" + sucessGIFCri;
-        }
-
-        else if (sum > echecValCri) {
-          chanceux = " | " + "**" + echecTXT + critique + "**" + echecGIFCri;
-        }
-        else if (sum <= n_stat) {
-          chanceux = " | " + "**" + sucessTXT + "**" + sucessGIF;
-        }
-
-        else if (sum > n_stat) {
-           chanceux = " | " + "**" + echecTXT + "**" + echecGIF;
-         }
-      }
-
-      else {
-        if (sum <= n_stat) {
-          chanceux = " | " + "**" + sucessTXT + "**" + sucessGIF;
-        }
-
-       else if (sum > n_stat) {
-          chanceux = " | " + "**" + echecTXT + "**" + echecGIF;
-        }
-      }
-
-      message_content = roll_user.mention() + ": " + dice.join(" & ") + " | Total = " + sum  + chanceux;
-      console.log("Le résultat est " + sum + ".");
-    }
-
-    else {
-    sum = dice[0];
+    console.log("Lancer de dé à " + n_sides + " faces avec " + n_stat + " de stats");
 
     if (n_sides == 6) {
       if(sum == sucessValCriSIX) {
@@ -113,13 +48,14 @@ dicetiny.on("message", function(message) {
     }
 
     else if (n_sides == 100) {
-      if(sum < sucessValCri) {
+      if (sum <= sucessValCri) {
         chanceux = " | " + "**" + sucessTXT + critique + "**" + sucessGIFCri;
       }
 
-      else if (sum > echecValCri) {
+      else if (sum >= echecValCri) {
         chanceux = " | " + "**" + echecTXT + critique + "**" + echecGIFCri;
       }
+
       else if (sum <= n_stat) {
         chanceux = " | " + "**" + sucessTXT + "**" + sucessGIF;
       }
@@ -129,32 +65,35 @@ dicetiny.on("message", function(message) {
        }
     }
 
-    else {
-      if (sum <= n_stat) {
-        chanceux = " | " + "**" + sucessTXT + "**" + sucessGIF;
-      }
-
-     else if (sum > n_stat) {
-        chanceux = " | " + "**" + echecTXT + "**" + echecGIF;
-      }
-    }
-
-     message_content = roll_user.mention() + ": " + sum  + chanceux;
-     console.log("Le résultat est " + sum + "." + chanceux);
-
-    if(message_content.length > 2000) {
-      var sum_message = "Texte trop long, résultat: " + sum;
-
-      dicetiny.sendMessage(channel_id, sum_message);
-      return;
-    }
+     message_content = user.mention() + ": " + sum  + chanceux;
+     console.log("Le résultat est " + sum + ".");
+     dicetiny.sendMessage(channel_id, message_content);
   }
-  dicetiny.sendMessage(channel_id, message_content);
-}
+});
+
+dicetiny.on("message", function(message) {
+  var channel_id = message.channel.id;
+  var match_data = message.content.match(/^\/roll d([1-9][0-9]*)/);
+
+  var message_content;
+  var user = message.author;
+
+  if(match_data) {
+    var n_sides = parseInt(match_data[1], 10);
+    var sum = roller.roll(1, n_sides);
+
+    console.log("Lancer de dé à " + n_sides + "faces");
+    message_content = user.mention() + ": " + sum;
+    console.log("Le résultat est " + sum + ".");
+    dicetiny.sendMessage(channel_id, message_content);
+  }
 });
 
 fs.readFile("config.json", function(err, data) {
   var auth_token =  JSON.parse(data).discord_token;
   dicetiny.loginWithToken(auth_token);
-  console.log("Bot en ligne");
+});
+
+dicetiny.on('ready', () => {
+  console.log(`Bot en ligne`);
 });
